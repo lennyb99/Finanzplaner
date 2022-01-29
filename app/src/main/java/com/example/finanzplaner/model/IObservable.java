618f54c2433ee3b6ed2a5a45504d0e351bfd6543
lennyb99 <lennogle@gmail.com>
2022-01-29 19:38:31 +0100

    MVC Architektur aufgebaut, ObserverEntwurf aufgebaut

---
+++ app/src/main/java/com/example/finanzplaner/model/IObservable.java
@@ -0,0 +1,13 @@
+package com.example.finanzplaner.model;
+
+public interface IObservable {
+
+    public void anmelden();
+    public void abmelden();
+
+    public void benachrichtigeBeobachter();
+
+    public void gibAktuelleDaten();
+
+
+}
---
+++ app/src/main/java/com/example/finanzplaner/model/finanzverwaltung/Ausgabe.java
@@ -0,0 +1,13 @@
+package com.example.finanzplaner.model.finanzverwaltung;
+
+
+enum ausgabeKategorien {}
+public class Ausgabe {
+
+    private String name;
+
+    public Ausgabe(){
+
+    }
+
+}
