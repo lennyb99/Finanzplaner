35e6384d83f6fae1a88d3b9efa20d0cc814e6057
lennyb99 <lennogle@gmail.com>
2022-02-16 18:29:22 +0100

    Small Fixes

--- app/src/main/java/com/example/finanzplaner/model/finanzverwaltung/Ausgabe.java
+++ app/src/main/java/com/example/finanzplaner/model/finanzverwaltung/Ausgabe.java
@@ -1,13 +1,17 @@
 package com.example.finanzplaner.model.finanzverwaltung;
 
 
+import java.util.Calendar;
+
 enum ausgabeKategorien {}
 public class Ausgabe {
 
     private String name;
+    private Calendar datum;
+    private boolean repetetiv;
 
     public Ausgabe(){
-
+        datum = Calendar.getInstance();
     }
 
 }
--- app/src/main/java/com/example/finanzplaner/model/finanzverwaltung/Verwaltung.java
+++ app/src/main/java/com/example/finanzplaner/model/finanzverwaltung/Verwaltung.java
@@ -7,6 +7,7 @@ public class Verwaltung {
 
     private List<Ausgabe> ausgaben;
     private List<Einnahme> einnahmen;
+    
 
     public Verwaltung(){
 
