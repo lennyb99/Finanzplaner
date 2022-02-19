4c02639dd47379a1e6a856e5bed6e99513cbc012
Anastasia <anastasia.efstatoglou@haw-hamburg.de>
2022-02-19 22:31:16 +0100

    added pie chart//merged

--- app/src/main/java/com/example/finanzplaner/view/Dashboard.java
+++ app/src/main/java/com/example/finanzplaner/view/Dashboard.java
@@ -2,13 +2,16 @@ package com.example.finanzplaner.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.finanzplaner.R;
+import com.example.finanzplaner.controller.DashboardController;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +21,17 @@ public class Dashboard extends AppCompatActivity implements IObserver{
     PieChart pieChart;
     PieData pieData;
     List<PieEntry> pieEntryList = new ArrayList<>();
+    DashboardController dbController;
+    FloatingActionButton eintraegeHinzufuegen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        eintraegeHinzufuegen = (FloatingActionButton) findViewById(R.id.hinzufuegen);
+        dbController = new DashboardController(this);
+
         pieChart = findViewById(R.id.pieChart);
         pieChart.setUsePercentValues(true);
         pieEntryList.add(new PieEntry(10,"India"));
@@ -38,4 +48,12 @@ public class Dashboard extends AppCompatActivity implements IObserver{
     public void update() {
 
     }
+    public FloatingActionButton getActionButton(){
+        return eintraegeHinzufuegen;
+    }
+
+    public void startNewActivity(Class dest){
+        startActivity(new Intent(Dashboard.this, dest));
+    }
+
 }
