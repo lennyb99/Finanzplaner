48886ab070add45ddb5c18d6810c8600adf214a3
mehrlean <merlin.abesha@outlook.de>
2022-02-20 23:45:43 +0100

    Added RecyclerView input ArrayLists (testing) Added method to pull contents for RecyclerView from DB

--- app/src/main/java/com/example/finanzplaner/view/DiagrammDetail.java
+++ app/src/main/java/com/example/finanzplaner/view/DiagrammDetail.java
@@ -5,8 +5,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import com.example.finanzplaner.R;
 import com.example.finanzplaner.model.finanzverwaltung.Einnahme;
+import com.example.finanzplaner.model.finanzverwaltung.Einnahmekategorie;
 import com.example.finanzplaner.model.finanzverwaltung.Verwaltung;
 
 import java.util.ArrayList;
@@ -14,9 +17,9 @@ import java.util.List;
 
 public class DiagrammDetail extends AppCompatActivity {
     RecyclerView recyclerView;
-    Verwaltung verwaltung = new Verwaltung();
-    List<Einnahme> einnahmen = verwaltung.getEinnahmen();
-
+    Verwaltung verwaltung;
+    List<Einnahme> einnahmen;
+    MyAdapter adapter;
 
     ArrayList<String> name;
     ArrayList<Float> betrag;
@@ -24,25 +27,7 @@ public class DiagrammDetail extends AppCompatActivity {
     ArrayList<String> kategorie;
     ArrayList<Boolean> wiederkehrend;
 
-    private void erstelleListen() {
 
-       for (int i = 0; i < einnahmen.size(); i++) {
-           name.add(einnahmen.get(i).getName());
-        }
-              for (int i = 0; i < einnahmen.size(); i++) {
-           betrag.add(einnahmen.get(i).getBetrag());
-        }
-              for (int i = 0; i < einnahmen.size(); i++) {
-           datum.add(einnahmen.get(i).getDatum());
-        }
-              for (int i = 0; i < einnahmen.size(); i++) {
-           kategorie.add(einnahmen.get(i).getEinnahmekategorie());
-        }
-              for (int i = 0; i < einnahmen.size(); i++) {
-           wiederkehrend.add(einnahmen.get(i).isWiederkehrend());
-        }
-
-    }
 
 
 
@@ -51,18 +36,53 @@ public class DiagrammDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        erstelleListen();
-        MyAdapter adapter = new MyAdapter(name,betrag,datum,kategorie,wiederkehrend);
-        recyclerView = findViewById(R.id.recyclerView);
-
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagrammdetail);
+        verwaltung = (Verwaltung) getIntent().getSerializableExtra("Verwaltung");
+        //einnahmen = verwaltung.getEinnahmen();
+        einnahmen = new ArrayList<>();
+        name = new ArrayList<>();
+        betrag = new ArrayList<>();
+        datum = new ArrayList<>();
+        kategorie = new ArrayList<>();
+        wiederkehrend = new ArrayList<>();
+
+        einnahmen.add(new Einnahme("Apfel",1,false,new Einnahmekategorie("Essen")));
+        einnahmen.add(new Einnahme("Birne",1,false,new Einnahmekategorie("Essen")));
+        einnahmen.add(new Einnahme("Kirschen",1,false,new Einnahmekategorie("Essen")));
+        einnahmen.add(new Einnahme("Kuchen",1,false,new Einnahmekategorie("Essen")));
+        erstelleListen();
+        adapter = new MyAdapter(name,betrag,datum,kategorie,wiederkehrend);
+        recyclerView = findViewById(R.id.recyclerview);
+        if (recyclerView == null){
+            Log.v("Adapter","Adapter");
+            return;
+        }
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 
+    private void erstelleListen() {
 
+        for (int i = 0; i < einnahmen.size(); i++) {
+            name.add(einnahmen.get(i).getName());
+        }
+        for (int i = 0; i < einnahmen.size(); i++) {
+            betrag.add(einnahmen.get(i).getBetrag());
+        }
+        for (int i = 0; i < einnahmen.size(); i++) {
+            datum.add(einnahmen.get(i).getDatum());
+        }
+        for (int i = 0; i < einnahmen.size(); i++) {
+            kategorie.add(einnahmen.get(i).getEinnahmekategorie());
+        }
+        for (int i = 0; i < einnahmen.size(); i++) {
+            wiederkehrend.add(einnahmen.get(i).isWiederkehrend());
+        }
 
+    }
 
 
 }
