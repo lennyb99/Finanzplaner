5b65d164cbbc99d6461bec4df16feb5398425dba
mehrlean <merlin.abesha@outlook.de>
2022-02-20 20:04:14 +0100

    Added RecyclerView and RecyclerView Adapter for DiagrammDetail

--- app/src/main/java/com/example/finanzplaner/view/DiagrammDetail.java
+++ app/src/main/java/com/example/finanzplaner/view/DiagrammDetail.java
@@ -1,13 +1,48 @@
 package com.example.finanzplaner.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import com.example.finanzplaner.R;
+import com.example.finanzplaner.model.finanzverwaltung.Einnahme;
+import com.example.finanzplaner.model.finanzverwaltung.Verwaltung;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiagrammDetail extends AppCompatActivity {
     RecyclerView recyclerView;
+    Verwaltung verwaltung = new Verwaltung();
+    List<Einnahme> einnahmen = verwaltung.getEinnahmen();
+
+
+    ArrayList<String> name;
+    ArrayList<Float> betrag;
+    ArrayList<String> datum;
+    ArrayList<String> kategorie;
+    ArrayList<Boolean> wiederkehrend;
+
+    private void erstelleListen() {
+
+       for (int i = 0; i < einnahmen.size(); i++) {
+           name.add(einnahmen.get(i).getName());
+        }
+              for (int i = 0; i < einnahmen.size(); i++) {
+           betrag.add(einnahmen.get(i).getBetrag());
+        }
+              for (int i = 0; i < einnahmen.size(); i++) {
+           datum.add(einnahmen.get(i).getDatum());
+        }
+              for (int i = 0; i < einnahmen.size(); i++) {
+           kategorie.add(einnahmen.get(i).getEinnahmekategorie());
+        }
+              for (int i = 0; i < einnahmen.size(); i++) {
+           wiederkehrend.add(einnahmen.get(i).isWiederkehrend());
+        }
+
+    }
 
 
 
@@ -16,9 +51,12 @@ public class DiagrammDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        erstelleListen();
+        MyAdapter adapter = new MyAdapter(name,betrag,datum,kategorie,wiederkehrend);
+        recyclerView = findViewById(R.id.recyclerView);
 
-        recyclerView = findViewById(R.id.legendeDiagramm);
-       MyAdapter myAdapter = new MyAdapter();
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagrammdetail);
     }
--- app/src/main/java/com/example/finanzplaner/view/MyAdapter.java
+++ app/src/main/java/com/example/finanzplaner/view/MyAdapter.java
@@ -1,44 +1,96 @@
 package com.example.finanzplaner.view;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.finanzplaner.R;
+import com.example.finanzplaner.model.finanzverwaltung.Ausgabe;
+import com.example.finanzplaner.model.finanzverwaltung.Einnahme;
+import com.example.finanzplaner.model.finanzverwaltung.Verwaltung;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-public class MyAdapter extends RecyclerView. Adapter<MyAdapter.ViewHolder>{
-
-
-
-     public MyAdapter() {
+public class MyAdapter extends RecyclerView. Adapter<MyAdapter.ViewHolder> {
 
 
+    Context context;
+    Verwaltung verwaltung = new Verwaltung();
+    ArrayList<String> name;
+    private ArrayList<Float> betrag;
+    private ArrayList<String> datum;
+    private ArrayList<String> einnahmekategorie;
+    private ArrayList<Boolean> wiederkehrend;
+    public MyAdapter(ArrayList<String> name, ArrayList<Float> betrag, ArrayList<String> datum, ArrayList<String> einnahmekategorie, ArrayList<Boolean> wiederkehrend) {
+        this.name = name;
+        this.betrag = betrag;
+        this.datum = datum;
+        this.einnahmekategorie = einnahmekategorie;
+        this.wiederkehrend = wiederkehrend;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        context = recyclerView.getContext();
+    }
+
+    private void ertelleListen(List einnahmen) {
 
 
-     }
+        List<Einnahme> name = verwaltung.getEinnahmen();
 
-     @NonNull
-     @Override
-     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-         return null;
-     }
+    }
 
-     @Override
-     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.activity_diagrammdetail, parent, false);
+        return new ViewHolder(view);
+    }
 
-     }
 
-     @Override
-     public int getItemCount() {
-         return 0;
-     }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-     public class ViewHolder extends RecyclerView.ViewHolder{
+        holder.titelTW.setText(name.get(position));
 
-        public ViewHolder (@NonNull View itemView){
-        super(itemView);
+        holder.betragTW.setText(String.valueOf(betrag.get(position)));
+        holder.datumTW.setText(datum.get(position));
+        holder.kategorieTW.setText(einnahmekategorie.get(position));
+        //holder.setText(einnahmekategorie[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+       return name.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titelTW, kategorieTW, datumTW, betragTW;
+        ImageView thumbnailTW;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titelTW = itemView.findViewById(R.id.RvTitel);
+            kategorieTW = itemView.findViewById(R.id.RvKategorie);
+            datumTW = itemView.findViewById(R.id.RvDatum);
+            betragTW = itemView.findViewById(R.id.RvBetrag);
+            thumbnailTW = itemView.findViewById(R.id.RvThumbnail);
         }
+    }
 }
-        }
