10e8943d415fb84c68dc89f708c2e3122d796187
mehrlean <merlin.abesha@outlook.de>
2022-02-20 03:24:57 +0100

    Testing of Database Object acess

--- app/src/main/java/com/example/finanzplaner/view/MainActivity.java
+++ app/src/main/java/com/example/finanzplaner/view/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 
 import com.example.finanzplaner.R;
 import com.example.finanzplaner.db.AppDatabase;
 import com.example.finanzplaner.db.DbData;
+import com.example.finanzplaner.model.finanzverwaltung.Einnahme;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
         DbData.db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "AppDatabase").allowMainThreadQueries().build();
         DbData.einnahmeDao = DbData.db.einnahmeDao();
+        DbData.einnahmeDao.deleteEinnahme(DbData.einnahmeDao.getEinnahme().get(0));
+        //DbData.einnahmeDao.insertEinnahmen(new Einnahme("Auto", true));
+
+        //Log.v("Test 1",DbData.einnahmeDao.getEinnahme().get(0).);
 
 
 
