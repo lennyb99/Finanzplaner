49cf57b30a3f1720ebfceb1f165a33fc62701933
lennyb99 <lennogle@gmail.com>
2022-02-20 14:22:14 +0100

    kategorien hinzugefügt

---
+++ app/src/main/java/com/example/finanzplaner/model/finanzverwaltung/Ausgabekategorie.java
@@ -0,0 +1,14 @@
+package com.example.finanzplaner.model.finanzverwaltung;
+
+public class Ausgabekategorie {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/finanzplaner/model/finanzverwaltung/Einnahmekategorie.java
@@ -0,0 +1,15 @@
+package com.example.finanzplaner.model.finanzverwaltung;
+
+public class Einnahmekategorie {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
