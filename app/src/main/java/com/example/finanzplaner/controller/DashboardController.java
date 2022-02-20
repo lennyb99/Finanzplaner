929e0f0bbf799d9a985faccd28a50dac6369e065
mehrlean <merlin.abesha@outlook.de>
2022-02-20 23:46:57 +0100

    Added OnclickListener for navigation Button to DiagrammDetails

--- app/src/main/java/com/example/finanzplaner/controller/DashboardController.java
+++ app/src/main/java/com/example/finanzplaner/controller/DashboardController.java
@@ -7,18 +7,32 @@ import android.view.View;
 
 import com.example.finanzplaner.view.Action;
 import com.example.finanzplaner.view.Dashboard;
+import com.example.finanzplaner.view.DiagrammDetail;
+import com.github.mikephil.charting.charts.PieChart;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DashboardController implements Controller{
 
 
     private FloatingActionButton eintraegeHinzufuegen;
+    private FloatingActionButton DiagrammDetails;
     private Dashboard dashboardView;
 
     public DashboardController(Dashboard dashboard){
 
         dashboardView = dashboard;
         eintraegeHinzufuegen = dashboardView.getActionButton();
+        DiagrammDetails = dashboardView.getActionButtonDetails();
+
+        DiagrammDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("Diagramm","Test");
+                dashboardView.startNewActivity(DiagrammDetail.class);
+
+            }
+        });
+
 
         eintraegeHinzufuegen.setOnClickListener(new View.OnClickListener() {
             @Override
